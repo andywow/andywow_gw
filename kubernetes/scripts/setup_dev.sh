921e73b895cca3777dd66fea92b72e706d965cd3
Andrey Susoev <andwow@gmail.com>
2018-05-13 16:42:24 +0300

    helm dep update command added to init scripts

--- kubernetes/scripts/setup_dev.sh
+++ kubernetes/scripts/setup_dev.sh
@@ -1,4 +1,8 @@
+#!/bin/sh
+helm dep update ../charts/proxy
 helm upgrade --install \
   --set nginx-ingress.controller.service.loadBalancerIP=35.234.123.86 proxy ../charts/proxy
+  helm dep update ../charts/log
 helm upgrade --install log ../charts/log
+helm dep update ../charts/monagent
 helm upgrade --install monagent --set baseDomain=dev.andywow.xyz ../charts/monagent
